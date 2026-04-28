9f1d1e99e8e76aec9c4f7a638de1af901b90a436
Deepanshu <deepanshu@users.noreply.github.com>
2026-04-28 14:54:36 +0000

    complete abstract Account class and create an enum for status of withdrawl

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,9 +1,12 @@
 package model;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public abstract class Account {
-    private String holderName;
-    private int accNumber;
-    private final static int accNumCounter = 1000;
+    private final String holderName;
+    private final int accNumber;
+    private static int accNumCounter = 1000;
     private double balance;
 
     public Account(String holderName, int balance) {
@@ -13,7 +16,45 @@ public abstract class Account {
         this.accNumber = accNumCounter++;
     }
 
-    public boolean deposit(double) {
+    public boolean deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            return true;
+        }
+         return false;
+    }
 
+    public WithdrawStatus withdraw(double amount) {
+        if (amount <= 0) {
+            return WithdrawStatus.MIN_BALANCE_VIOLATION;
+        }
+
+        WithdrawStatus status = canWithDraw(amount);
+
+        if (status == WithdrawStatus.SUCCESS) {
+            this.balance -= amount;
+        }
+
+        return status;
+    }
+
+    protected abstract WithdrawStatus canWithDraw(double amount);
+
+    public void displayDetails() {
+        NumberFormat indianCurrency = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        String balanceInRupees = indianCurrency.format(this.balance);
+        System.out.printf("Holder Name: %s | Account Number: %d | Balance: %s %n", this.holderName, this.accNumber, balanceInRupees);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccNumber() {
+        return accNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
     }
 }
---
+++ src/model/WithdrawStatus.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum WithdrawStatus {
+    SUCCESS,
+    INSUFFICIENT_BALANCE,
+    MIN_BALANCE_VIOLATION,
+    ACCOUNT_NOT_FOUND
+}
